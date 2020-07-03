c4c7dffd3877c453bd0a7e5fe1043f72580f1217
tarikavdibasic <59517286+tarikavdibasic@users.noreply.github.com>
2020-07-03 13:35:38 +0200

    Updated the StringQueueTest.java file with setup() and tearDown() methods

--- src/test/java/at/fhj/iit/StringQueueTest.java
+++ src/test/java/at/fhj/iit/StringQueueTest.java
@@ -3,10 +3,42 @@ package at.fhj.iit;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
+/**
+ * Testing the StringQueue class
+ *
+ * @author Tarik Avdibasic
+ * @author Martin Feichtinger
+ *
+ * @version %I%, %G%
+ */
+@DisplayName("Testing StringQueue")
 class StringQueueTest {
+     private StringQueue emptyQueueObject,queueObject;
 
-   // TODO Write your own tests
+    /**
+     * Inits the StringQueue object for tests
+     * The setup phase
+     */
+    @BeforeEach
+    public void setup(){
+        emptyQueueObject = new StringQueue();
+        queueObject = new StringQueue(4);
+    }
+
+    /**
+     * Teardown method to reset/clear the values
+     */
+    @AfterEach
+    public void tearDown(){
+        emptyQueueObject = null;
+        queueObject = null;
+    }
+
+
+
 
 }
